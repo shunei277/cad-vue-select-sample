7e588774ea3e0155471a835d328f7084ef79ef16
Shunei <108961459+shunei277@users.noreply.github.com>
2022-08-31 17:55:44 +0900

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import vSelect from 'vue-select'
+import {ref} from "vue";
+
+const values = ref([])
+const options = [
+  {id: '1', label: '日本，東京'},
+  {id: '2', label: '日本，広島'},
+  {id: '3', label: '日本，福岡'},
+  {id: '4', label: 'アメリカ，カリフォルニア'},
+  {id: '5', label: 'アメリカ，ニューヨーク'},
+]
+const onSearch = (text: string, loading: (loading: boolean) => void) => {
+  loading(true)
+  console.log(text)
+  loading(false)
+}
+</script>
+
+<template>
+  <v-select class="style-chooser w-full border-0" :options="options" v-model="values" multiple @search="onSearch">
+    <template #selected-option-container="{ option, deselect, multiple, disabled }" >
+      <div class="bg-blue-200 px-2 py-1 my-1 mr-1 rounded">
+        {{option.label}}
+        <span class="ml-1 cursor-pointer" @click="deselect(option)">X</span>
+      </div>
+    </template>
+  </v-select>
+</template>
+
+<style>
+.style-chooser .vs__search::placeholder,
+.style-chooser .vs__dropdown-toggle {
+  background: #dfe5fb;
+  border: none;
+  color: #394066;
+  text-transform: lowercase;
+  font-variant: small-caps;
+}
+
+.style-chooser .vs__clear,
+.style-chooser .vs__open-indicator {
+  fill: #394066;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import 'vue-select/dist/vue-select.css';
+
+createApp(App).mount('#app')
